4052722f298431e6b8bbc0d0faa60245bbbc72ee
Keiyu <Nickcompanyofficial@gmail.com>
2025-09-09 18:45:15 +0200

    commit 20

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-from llama_cpp import Llama
+import ollama
 
-llm = Llama(model_path="path_al_modello/ggml-model-q4_0.bin")
+def classify_domain(domain):
 
+    # Define the model and the input prompt
+    prompt = f"You are a traffic monitoring assistant. Tell me the name in ONE WORD of the parent company / service / application to which this domain belongs: {domain}"
+
+    # Send the query to the model
+    response = client.generate(model="gemma3", prompt=prompt)
+    print(response.response)
+
+# Initialize the Ollama client
+client = ollama.Client()
+
+# Initialize domains
 domains = ["app.lefrecce.it", "dpm.demdex.net", "fstechnologyspa.data.adobedc.net"]
-prompt = f"Dimmi solo i nomi dei processi che generano questi domini: {', '.join(domains)}"
 
-response = llm(prompt, max_tokens=50)
-print(response['choices'][0]['text'])
-
-PROVA SINCO
+for d in domains:
+    classify_domain(d)
